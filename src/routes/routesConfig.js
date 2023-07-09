02e7c9c8192d144c19d2e901029af342802cb5ef
Byron Farrell <byronfarrell22@gmail.com>
2023-07-09 22:21:36 +0100

    Request body validation

---
+++ src/routes/routesConfig.js
@@ -0,0 +1,58 @@
+const authControllers = require("../controllers/authControllers");
+const userControllers = require("../controllers/userControllers");
+
+// TODO: unit test to test methid field is correct (compare with list of valid methods)
+const routes = {
+    // Auth
+    login: { path: '/auth/login', method: 'POST', name: 'login' },
+    register: { path:'/auth/register', method: 'POST', name: 'register' },
+
+    // Users
+    listUsers: { path:'/user', method: 'GET', name: 'listUsers' },
+    getUser: { path:'/user/:userId', method: 'GET', name: 'getUser' },
+    patchUser: { path:'/user/:userId', method: 'PATCH', name: 'patchUser' },
+    deleteUser: { path:'/user/:userId', method: 'DELETE', name: 'deleteUser' }
+}
+
+// TODO: unit test to check that all paths have a validation object
+const validations = {
+
+    [routes.login.name]: {
+        fields: {
+            required: ['username', 'password']
+        }
+    },
+
+    [routes.register.name]: {
+        fields: {
+            required: ['username', 'password']
+        }
+    },
+
+    [routes.getUser.name]: {
+        fields: {
+            required: []
+        }
+    },
+
+    [routes.listUsers.name]: {
+        fields: {
+            required: []
+        }
+    },
+
+    [routes.patchUser.name]: {
+        fields: {
+            required: []
+        }
+    },
+
+    [routes.deleteUser.name]: {
+        fields: {
+            required: []
+        }
+    }
+}
+
+exports.routes = routes
+exports.validations = validations
